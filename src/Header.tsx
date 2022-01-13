3cd42b78aff0613dd244221e2674e3c83c6b2781
Kellen Parker <kellenrparker@gmail.com>
2022-01-12 19:39:46 -0600

    initial commit

---
+++ src/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'
+
+export default function Header() {
+	return (
+		<div className="header">
+			<h1 className="title">On the Ropes</h1>
+			<button className="headButton">Test</button>
+			<button className="headButton">Test2</button>
+			<button className="headButton">Test3</button>
+			<button className="headButton">Test4</button>
+		</div>
+	);
+}
+
