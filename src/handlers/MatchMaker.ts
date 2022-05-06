451f07d3e3858d6d8ae881397010d95a1f11f213
Kellen Parker <kellenrparker@gmail.com>
2022-05-06 09:37:32 -0500

    preliminary matchmaking done for all types

--- src/handlers/MatchMaker.ts
+++ src/handlers/MatchMaker.ts
@@ -20,9 +20,10 @@ function MatchMaker(cards: Card[], roster: Fighter[][]): void{
                 for(let k = 0; k < wcSize; k++){
                     if(j === k) continue;
                     if(roster[i][k].hasFight) continue;
+                    if(roster[i][j].type === 3) continue;
 
                     candidate = (roster[i][k].popularity / (100.0 / .4)) + (roster[i][k].overall / (100.0 / .3)) + (roster[i][k].belts / (3 / .3));
-                    candidate += _.random(-.05, .05, true);
+                    candidate = _.clamp(candidate + _.random(-.05, .05, true), 0, 100);
 
                     if(candidate > score) {
                         score = candidate;
@@ -32,32 +33,65 @@ function MatchMaker(cards: Card[], roster: Fighter[][]): void{
             }
             else if(type === 1){ // prospects
                 for(let k = 0; k < wcSize; k++){
+                    if(roster[i][k].type !== 3) continue;
+                    if(j === k) continue;
+                    if(roster[i][k].hasFight) continue;
+                    if(roster[i][k].overall >= roster[i][j].overall) continue;
 
-                    
+                    candidate = (.8 / (Math.abs(roster[i][k].overall - (roster[i][j].overall - 5)) + 1));
+                    candidate = _.clamp(candidate + _.random(-.05, .05, true), 0, 100);
+
+                    if(candidate > score){
+                        score = candidate;
+                        scoreInd = k;
+                    }
                 }
             }
             else if(type === 2){ // legends
                 for(let k = 0; k < wcSize; k++){
+                    if(j === k) continue;
+                    if(roster[i][k].hasFight) continue;
 
-                    
+                    candidate = (roster[i][k].popularity / (100.0 / .5)) - (roster[i][k].overall / (100.0 / .2)) + (roster[i][k].belts / (3 / .3));
+                    candidate = _.clamp(candidate + _.random(-.05, .05, true), 0, 100);
+
+                    if(candidate > score){
+                        score = candidate;
+                        scoreInd = k;
+                    }
                 }
             }
             else if(type === 3){ // cans
                 for(let k = 0; k < wcSize; k++){
-
+                    if(roster[i][k].type !== 3) continue;
+                    if(j === k) continue;
+                    if(roster[i][k].hasFight) continue;
                     
+                    candidate = (.4 / (Math.abs(roster[i][k].overall - roster[i][j].overall) + 1));
+                    candidate = _.clamp(candidate + _.random(-.05, .05, true), 0, 100);
+
+                    if(candidate > score){
+                        score = candidate;
+                        scoreInd = k;
+                    }
                 }
             }
             else if(type === 4){ // average
                 for(let k = 0; k < wcSize; k++){
-
+                    if(roster[i][k].type !== 4) continue;
+                    if(j === k) continue;
+                    if(roster[i][k].hasFight) continue;
                     
+                    candidate = (.4 / (Math.abs(roster[i][k].overall - roster[i][j].overall) + 1));
+                    candidate = _.clamp(candidate + _.random(-.05, .05, true), 0, 100);
+
+                    if(candidate > score){
+                        score = candidate;
+                        scoreInd = k;
+                    }
                 }
             }
-            if(i === 0 && type === 0){
-                console.log(score);
-                console.log(roster[i][scoreInd]);
-            }
+            
         }
     }
 
