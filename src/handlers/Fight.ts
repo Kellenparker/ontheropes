edb7e58959b178ba1e1016703af338f1bdebddbb
Kellen Parker <kellenrparker@gmail.com>
2022-06-08 10:27:39 -0500

    reworked fight system to improve accuracy

--- src/handlers/Fight.ts
+++ src/handlers/Fight.ts
@@ -1,20 +1,73 @@
 import _ from "lodash";
 import { Match, Result } from "./CardHandler";
 
-function Fight(match: Match): Result{
-    
-    let dif = (match.fighterOne.overall - match.fighterTwo.overall) / 5;
+function Fight(match: Match): Result {
     var winner;
     let result: Result;
+    let finish: number; //0: KO, 1: TKO, 2: RTD, 3: DSQ
+    let round: number;
 
     match.fighterOne.fights++;
     match.fighterTwo.fights++;
 
-    if(_.random(-25, 25, true) > dif) winner = 0;
-    else winner = 1;
+    let dif = {
+        reach: match.fighterOne.reach - match.fighterTwo.reach,
+        height: match.fighterOne.height - match.fighterTwo.height,
+        stamina: match.fighterOne.stamina - match.fighterTwo.stamina,
+        power: match.fighterOne.power - match.fighterTwo.power,
+        chin: match.fighterOne.chin - match.fighterTwo.chin,
+        speed: match.fighterOne.speed - match.fighterTwo.speed,
+        timing: match.fighterOne.timing - match.fighterTwo.timing,
+        defense: match.fighterOne.defense - match.fighterTwo.defense,
+        footwork: match.fighterOne.footwork - match.fighterTwo.footwork,
+    };
 
-    if(winner === 0){
-        if(match.fighterTwo.belts > 0){
+    let fOneKoChance =
+        match.fighterOne.power / (100 / 0.3) +
+        match.fighterOne.timing / (100 / 0.3) +
+        match.fighterOne.stamina / (100 / 0.3) -
+        (match.fighterTwo.chin / (100 / 0.3) +
+            match.fighterTwo.defense / (100 / 0.3) +
+            match.fighterTwo.stamina / (100 / 0.3));
+
+    let fTwoKoChance =
+        match.fighterTwo.power / (100 / 0.3) +
+        match.fighterTwo.timing / (100 / 0.3) +
+        match.fighterTwo.stamina / (100 / 0.3) -
+        (match.fighterOne.chin / (100 / 0.3) +
+            match.fighterOne.defense / (100 / 0.3) +
+            match.fighterOne.stamina / (100 / 0.3));
+
+    let fOneKo = fOneKoChance / 3 > _.random(0, 1, true);
+    let fTwoKo = fTwoKoChance / 3 > _.random(0, 1, true);
+    if (fOneKo && !fTwoKo) {
+        finish = 0;
+        round = _.random(1, match.rounds, false);
+        winner = 0;
+    } else if (!fOneKo && fTwoKo) {
+        finish = 0;
+        round = _.random(1, match.rounds, false);
+        winner = 1;
+    } else if (fOneKo && fTwoKo) {
+        finish = 0;
+        round = _.random(1, match.rounds, false);
+        winner = _.random(0, 1, true) > .5 ? 0 : 1;
+    } else {
+        let physDif = dif.height + dif.reach + dif.stamina + dif.chin + dif.power;
+        let skillDif = dif.timing + dif.defense + dif.speed + dif.footwork;
+        let sway = 3;
+
+        let trueDif = (physDif / .6)+ skillDif;
+
+        if(trueDif > 0) winner = _.random(0 - (sway / trueDif), trueDif - (sway / trueDif), true) > 0 ? 0 : 1;
+        else winner = _.random(trueDif + (sway / trueDif), 0 + (sway / trueDif), true) > 0 ? 1 : 0;
+
+        finish = -1;
+        round = -1;
+    }
+
+    if (winner === 0) {
+        if (match.fighterTwo.belts > 0) {
             match.fighterOne.belts += match.fighterTwo.belts;
             match.fighterTwo.belts = 0;
         }
@@ -31,9 +84,9 @@ function Fight(match: Match): Result{
         match.fighterOne.wins += 1;
         match.fighterTwo.losses += 1;
 
-        if(match.fighterOne.streak > 0) match.fighterOne.streak++;
+        if (match.fighterOne.streak > 0) match.fighterOne.streak++;
         else match.fighterOne.streak = 1;
-        if(match.fighterTwo.streak < 0) match.fighterTwo.streak--;
+        if (match.fighterTwo.streak < 0) match.fighterTwo.streak--;
         else match.fighterTwo.streak = -1;
 
         match.fighterOne.hasFight = false;
@@ -44,11 +97,11 @@ function Fight(match: Match): Result{
             fighterTwo: match.fighterTwo,
             matchId: {
                 fone: match.fighterOne.id,
-                ftwo: match.fighterTwo.id
+                ftwo: match.fighterTwo.id,
             },
             weight: match.weight,
-            title: match.title
-        }
+            title: match.title,
+        };
         match.fighterOne.results.push({
             opponent: {
                 name: match.fighterTwo.firstName + " " + match.fighterTwo.lastName,
@@ -60,7 +113,11 @@ function Fight(match: Match): Result{
             num: match.fighterOne.fights,
             dateStr: match.dateStr,
             win: true,
-            title: match.title
+            finish: {
+                style: finish,
+                round: round,
+            },
+            title: match.title,
         });
         match.fighterTwo.results.push({
             opponent: {
@@ -73,11 +130,14 @@ function Fight(match: Match): Result{
             num: match.fighterTwo.fights,
             dateStr: match.dateStr,
             win: false,
-            title: match.title
+            finish: {
+                style: finish,
+                round: round,
+            },
+            title: match.title,
         });
-    }
-    else {
-        if(match.fighterOne.belts > 0){
+    } else {
+        if (match.fighterOne.belts > 0) {
             match.fighterTwo.belts += match.fighterOne.belts;
             match.fighterOne.belts = 0;
         }
@@ -94,9 +154,9 @@ function Fight(match: Match): Result{
         match.fighterOne.losses += 1;
         match.fighterTwo.wins += 1;
 
-        if(match.fighterOne.streak < 0) match.fighterOne.streak--;
+        if (match.fighterOne.streak < 0) match.fighterOne.streak--;
         else match.fighterOne.streak = -1;
-        if(match.fighterTwo.streak > 0) match.fighterTwo.streak++;
+        if (match.fighterTwo.streak > 0) match.fighterTwo.streak++;
         else match.fighterTwo.streak = 1;
 
         match.fighterOne.hasFight = false;
@@ -107,11 +167,11 @@ function Fight(match: Match): Result{
             fighterTwo: match.fighterOne,
             matchId: {
                 fone: match.fighterTwo.id,
-                ftwo: match.fighterOne.id
+                ftwo: match.fighterOne.id,
             },
             weight: match.weight,
-            title: match.title
-        }
+            title: match.title,
+        };
         match.fighterOne.results.push({
             opponent: {
                 name: match.fighterTwo.firstName + " " + match.fighterTwo.lastName,
@@ -123,7 +183,11 @@ function Fight(match: Match): Result{
             num: match.fighterOne.fights,
             dateStr: match.dateStr,
             win: false,
-            title: match.title
+            finish: {
+                style: finish,
+                round: round,
+            },
+            title: match.title,
         });
         match.fighterTwo.results.push({
             opponent: {
@@ -136,12 +200,15 @@ function Fight(match: Match): Result{
             num: match.fighterTwo.fights,
             dateStr: match.dateStr,
             win: true,
-            title: match.title
+            finish: {
+                style: finish,
+                round: round,
+            },
+            title: match.title,
         });
     }
 
     return result;
-
 }
 
 export default Fight;
